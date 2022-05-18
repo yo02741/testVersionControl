aed103df906658d387b6eb4d14df82cda479217b
t13768 <t13768@ms.sltung.com.tw>
2022-05-18 11:45:13 +0800

    feat: new1

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
+  <span>Finish new1</span>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
